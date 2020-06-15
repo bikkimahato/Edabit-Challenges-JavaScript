c3cc667d656c8d41eaae8a511aa2781012decdf8
Bikki Mahato <42492682+bikkimahato@users.noreply.github.com>
2020-06-15 10:41:23 +0530

    Solved Extract City Facts (Very Easy)

---
+++ Very Easy/extract_city_facts.js
@@ -0,0 +1,6 @@
+// Extract City Facts
+// https://edabit.com/challenge/FzDAgSR84zeyRa278
+
+function cityFacts(city) {
+    return `${city.name} has a population of ${city.population} and is situated in ${city.continent}`;
+}
