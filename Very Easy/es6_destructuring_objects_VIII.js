d6c6272f8dae522e950acfe4a9766ab294088ff3
Bikki Mahato <42492682+bikkimahato@users.noreply.github.com>
2020-06-15 10:44:39 +0530

    Solved ES6: Destructuring Objects VIII (Very Easy)

---
+++ Very Easy/es6_destructuring_objects_VIII.js
@@ -0,0 +1,5 @@
+// ES6: Destructuring Objects VIII
+// https://edabit.com/challenge/7GWG3z27pKqJRdPaM
+
+const user = { name: "John", email: "john@example.com", city: "Phoenix", state: "AZ", country: "USA" }
+const str = `({ name, email, ...rest} = user ).toString()`
