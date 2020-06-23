f370209cd2e810d4254f4edd4f8643c272ca9942
Bikki Mahato <42492682+bikkimahato@users.noreply.github.com>
2020-06-23 18:14:34 +0530

    Solved Hello; Hello World; World (Very Easy)

---
+++ Very Easy/hello_hello_world_world.js
@@ -0,0 +1,11 @@
+// Hello; Hello World; World
+// https://edabit.com/challenge/uJqrcYkz4NvYi9eCY
+
+function helloWorld(num) {
+    if (num % 15 === 0)
+        return "Hello World"
+    else if (num % 3 === 0)
+        return "Hello"
+    else if (num % 5 === 0)
+        return "World"
+}
