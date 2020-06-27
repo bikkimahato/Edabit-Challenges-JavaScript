9702ce8476ee0f699beed571f367563d95378488
bikkimahato <bikkimahato@gmail.com>
2020-06-27 13:47:39 +0530

    Solved Basic Arithmetic Operations on a String Number (Hard)

---
+++ Hard/basic_arithmetic_operations_on_a_string_number.js
@@ -0,0 +1,6 @@
+// Basic Arithmetic Operations on a String Number
+// https://edabit.com/challenge/T5MRvCGsZgk5QzCcZ
+
+function arithmeticOperation(n) {
+    return eval(n) === Infinity ? -1 : eval(n)
+}
