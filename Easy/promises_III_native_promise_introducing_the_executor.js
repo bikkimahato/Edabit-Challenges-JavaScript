640fe5b32b51efb06bfc727210988339f0ea7f2b
Bikki Mahato <>
2020-08-07 20:48:59 +0530

    Solved Promises III: Native Promise, Introducing the Executor (Easy)

---
+++ Easy/promises_III_native_promise_introducing_the_executor.js
@@ -0,0 +1,8 @@
+// Promises III: Native Promise, Introducing the Executor
+// https://edabit.com/challenge/8kTQqoWYQXRsKuYEf
+
+let promise = new Promise((resolve) => {
+    setTimeout(() => {
+        resolve("edabit")
+    }, 1000)
+})
