bd9d1ff1e7f6a56d7e770e8be7eb0666e53b2cee
Bikki Mahato <68054119+bikkimahatonumen@users.noreply.github.com>
2020-08-07 20:44:49 +0530

    Solved Is it Time for Milk and Cookies? (Easy)

---
+++ Easy/is_it_time_for_milk_and_cookies.js
@@ -0,0 +1,6 @@
+// Is it Time for Milk and Cookies?
+// https://edabit.com/challenge/hPWnaSckJke5FXNEH
+
+function timeForMilkAndCookies(date) {
+    return date.getMonth() === 11 && date.getDate() === 24
+}
